53f546566974a40c428cdd386e1008ec97cc6425
addoumi mohammed <addoumi.mohammed@gmail.com>
2022-07-02 15:38:45 +0100

    SMA-002 : completion de user EndPoint

--- src/main/java/com/micro/schoolmanagement/apis/UserApi.java
+++ src/main/java/com/micro/schoolmanagement/apis/UserApi.java
@@ -2,26 +2,51 @@ package com.micro.schoolmanagement.apis;
 
 import com.micro.schoolmanagement.models.User;
 import com.micro.schoolmanagement.repositories.UserRepository;
+import com.micro.schoolmanagement.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api-user/v1")
+@AllArgsConstructor
 public class UserApi {
 
     private final UserRepository userRepository;
+
+    private final UserService userService;
 
     @GetMapping("list")
     public List<User> getUsers(){
         return new ArrayList<>(userRepository.findAll());
     }
 
+    @PostMapping("save")
+    public User addUser(@RequestBody @Valid User user,Errors errors){
+        if(errors.hasErrors()){
+            errors.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .forEach(System.out::println);
+        }
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("{id}")
+    public User deleteUserById(@PathVariable Long id){
+        Optional<User> user = userRepository.findById(id);
+        userRepository.delete(user
+                            .orElseThrow(()->new IllegalArgumentException("no user with the id "+ id + " found")));
+        return user.orElse(null);
+    }
+
+    @GetMapping("get")
+    public String fetchUserByUserNameAndPassword(@RequestParam String userName, @RequestParam String password){
+        return userService.login(userName,password);
+    }
 }
--- src/main/java/com/micro/schoolmanagement/models/User.java
+++ src/main/java/com/micro/schoolmanagement/models/User.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 import static java.time.LocalDate.now;
@@ -21,8 +23,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Size(min = 5 , message = "firstName should be at least 5 characters length")
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank
+    @Size(min = 6, message = "username should be at least 6 characters long")
+    private String userName;
+    private String password;
     private LocalDate dateOfBirth;
     private LocalDate createdAt;
     private LocalDate updatedAt;
