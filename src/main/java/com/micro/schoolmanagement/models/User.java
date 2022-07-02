5ac11014ac4e792df2edffc27b86b0ecfde7516a
addoumi mohammed <addoumi.mohammed@gmail.com>
2022-07-02 16:53:05 +0100

    SMA - 003 : Migration de H2 vers Postgresql

--- src/main/java/com/micro/schoolmanagement/models/User.java
+++ src/main/java/com/micro/schoolmanagement/models/User.java
@@ -9,8 +9,9 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import static java.time.LocalDate.now;
+import static java.time.LocalDateTime.*;
 
 @Entity
 @Table(name = "sm_user")
@@ -23,17 +24,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "firstname")
     @Size(min = 5 , message = "firstName should be at least 5 characters length")
     private String firstName;
+    @Column(name = "lastname")
     @NotBlank
     private String lastName;
+    @Column(name = "username")
     @NotBlank
     @Size(min = 6, message = "username should be at least 6 characters long")
     private String userName;
     private String password;
+    @Column(name = "dateofbirth")
     private LocalDate dateOfBirth;
-    private LocalDate createdAt;
-    private LocalDate updatedAt;
+    @Column(name = "createdat")
+    private LocalDateTime createdAt;
+    @Column(name = "updatedat")
+    private LocalDateTime updatedAt;
 
     @PrePersist
     public void prePersist(){
